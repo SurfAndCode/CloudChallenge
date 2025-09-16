66e907c71550495b0fa6ede9b6a85b5b387335fa
Zoltan Olasz <zoltan.olasz.tech@gmail.com>
2025-09-16 14:09:42 +1000

    Path Fix

--- Smoke/cypress.config.js
+++ Smoke/cypress.config.js
@@ -9,7 +9,7 @@ module.exports = defineConfig({
     requestTimeout: 8000,
     supportFile: false,
     setupNodeEvents(on, config) {
-      config.env.HEALTH_PATH = process.env.CYPRESS_HEALTH_PATH || config.env.HEALTH_PATH || "/api/health";
+      config.env.HEALTH_PATH = process.env.CYPRESS_HEALTH_PATH || config.env.HEALTH_PATH || "/health";
       config.env.ALLOW_MUTATION = (process.env.CYPRESS_ALLOW_MUTATION || "true").toLowerCase() === "true";
       return config;
     },
